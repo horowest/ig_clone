17eaf38d34b285ad30514569d194751978da9aed
horowest <akashvevo5@gmail.com>
2020-05-31 08:11:32 +0530

    site.db will now be automatically created

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask import Flask
@@ -6,6 +7,9 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '2951b9d5bb58fe1fac16d872533168aa'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+if not os.path.exists('site.db'):
+    db.create_all()
+
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
