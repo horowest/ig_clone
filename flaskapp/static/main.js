9c0e4b2fdb49a246696b7f28f788f928c357ec55
horowest <akashvevo5@gmail.com>
2020-06-13 18:53:19 +0530

    fixed some bugs and moved around functions

--- flaskapp/static/main.js
+++ flaskapp/static/main.js
@@ -1,16 +1,106 @@
+// handle comments 
+function comment_handler() {
+                        
+    var post_id = $(this).attr('id').split('-')[1];
+    var msg = $("#comment-on-" + post_id).val();
+
+    // alert('comment ' + msg);
+    msg = $.trim(msg);
+    // aler(msg);
+    // return;
+    if(msg == '') {
+        alert("Cannot post empty comment");
+        return false;
+    }
+    var url = "/comment/" + post_id;
+    $.post($SCRIPT_ROOT + url,
+    {
+        msg: msg
+    },
+    function(data, success) {
+        //alert(data.content + '\n ' + success);
+        $("#comment-on-" + post_id).val('');
+
+        var comment = "<span class='comment' id='c" + data.cid + "'><a class='text-dark user-link' href='" 
+            + data.user_url + "'>" + data.username + "</a> " + data.content 
+            + "<small class='float-right delete-comment' id='comment-" + data.cid 
+            + "'><a class='text-danger' href='#'>delete</a></small>"
+            + "</span>";
+
+        $("#comments-on-" + post_id).append(comment);
+        $("#c-" + post_id).attr("disabled", true);
+
+        $('#comment-' + data.cid).click(function(e) {
+            e.preventDefault();
+    
+            var com_id = $(this).attr('id').split('-')[1];
+            var url = "/comment/" + com_id + "/delete";
+    
+            $.get($SCRIPT_ROOT + url, 
+            function(data, success) {
+                // alert('deleted');
+                $("#c" + com_id).remove();
+            });
+        });
+    });
+}
+
+// enable or diable post button
+function enable_disable_button() {
+    const com_id = this.id.split('-')[2];
+    let com = this.value;
+
+    if (com.length > 0)
+        document.querySelector(`#c-${com_id}`).disabled = false;
+    else 
+        document.querySelector(`#c-${com_id}`).disabled = true;
+}
+
+
+// like a post
+function like_post() {
+    var post_id = $(this).attr('id').split('-')[1];
+    var url = "/post/" + post_id + "/like";
+    $.get($SCRIPT_ROOT + url, 
+    function(data, success) {
+        // alert(data.result + " \nstatus: " + success);
+        var likeId = "#like-" + post_id;
+        var olddata = $(likeId).text();
+        $(likeId).text(data.result);
+        
+        // update like icon
+        if(data.result > olddata) {
+            $('#l-' + post_id).addClass("liked");
+            $('#l-' + post_id).html("<i class='fas fa-heart'></i>");
+        }
+        else {
+            $('#l-' + post_id).removeClass("liked");
+            $('#l-' + post_id).html("<i class='far fa-heart'></i>");
+        }
+    });
+}
+
+
+// delete comment
+function delete_comment(e) {
+    e.preventDefault();
+
+    var com_id = $(this).attr('id').split('-')[1];
+    var url = "/comment/" + com_id + "/delete";
+
+    $.get($SCRIPT_ROOT + url, 
+    function(data, success) {
+        // alert('deleted');
+        $("#c" + com_id).remove();
+    });
+}
+
+
 $(document).ready(function() {
 
     $('.post-comment').attr("disabled", true);
 
-    $('.make-comment').on('keyup', function() {
-        const com_id = $(this).attr('id').split('-')[2];
-        let com = $(this).val();
-
-        if (com.length > 0)
-            $("#c-" + com_id).attr("disabled", false);
-        else 
-            $("#c-" + com_id).attr("disabled", true);
-    });
+    $('.make-comment').on('keyup', enable_disable_button);
 
     // follow user
     $("#flw").click(function() {
@@ -21,7 +111,7 @@ $(document).ready(function() {
                 username: $("#user").text() 
             }, 
             function(data, success) {
-                //alert(data.result + " status: " + success);
+                // alert(data.result + " status: " + success);
                 $("#flw").text('Following');
                 $("#flw").removeClass('btn-primary');
                 $("#flw").addClass('btn-secondary');
@@ -33,7 +123,7 @@ $(document).ready(function() {
                 username: $("#user").text() 
             }, 
             function(data, success) {
-                //alert(data.result + " status: " + success);
+                // alert(data.result + " status: " + success);
                 $("#flw").text('Follow');
                 $("#flw").removeClass('btn-secondary');
                 $("#flw").addClass('btn-primary');
@@ -66,6 +156,19 @@ $(document).ready(function() {
                 $('#s-'+user_id).addClass('text-secondary');
             });
         }
+        else if(follows_or_not == 'Following') {
+            // alert('to follow');
+            $.post($SCRIPT_ROOT + "/unfollow",
+            { 
+                username: username
+            },
+            function(data, success) {
+                // alert(data.result + " status: " + success);
+                $('#s-'+user_id).text('Follow');
+                $('#s-'+user_id).removeClass('text-secondary');
+                $('#s-'+user_id).addClass('text-primary');
+            });
+        }
 
     });
 
@@ -73,87 +176,11 @@ $(document).ready(function() {
     $("textarea").val('');
 
     // like a post
-    $(".like").click(function() {
-        
-        var post_id = $(this).attr('id').split('-')[1];
-        var url = "/post/" + post_id + "/like";
-        $.get($SCRIPT_ROOT + url, 
-        function(data, success) {
-            // alert(data.result + " \nstatus: " + success);
-            var likeId = "#like-" + post_id;
-            var olddata = $(likeId).text();
-            $(likeId).text(data.result);
-            
-            // update like icon
-            if(data.result > olddata) {
-                $('#' + post_id).addClass("liked");
-                $('#' + post_id).html("<i class='fas fa-heart'></i>");
-            }
-            else {
-                $('#' + post_id).removeClass("liked");
-                $('#' + post_id).html("<i class='far fa-heart'></i>");
-            }
-        });
-    });
+    $(".like").click(like_post);
 
     // post new comment
-    $(".post-comment").click(function() {
-            
-        var post_id = $(this).attr('id').split('-')[1];
-        var msg = $("#comment-on-" + post_id).val();
-
-        // alert('comment ' + msg);
-        msg = $.trim(msg);
-        // aler(msg);
-        // return;
-        if(msg == '') {
-            alert("Cannot post empty comment");
-            return false;
-        }
-        var url = "/comment/" + post_id;
-        $.post($SCRIPT_ROOT + url,
-        {
-            msg: msg
-        },
-        function(data, success) {
-            //alert(data.content + '\n ' + success);
-            $("#comment-on-" + post_id).val('');
-
-            var comment = "<span class='comment' id='c" + data.cid + "'><a class='text-dark user-link' href='" 
-                + data.user_url + "'>" + data.username + "</a> " + data.content 
-                + "<small class='float-right delete-comment' id='comment-" + data.cid 
-                + "'><a class='text-danger' href='#'>delete</a></small>"
-                + "</span>";
-
-            $("#comments-on-" + post_id).append(comment);
-            $("#c-" + post_id).attr("disabled", true);
-
-            $('#comment-' + data.cid).click(function(e) {
-                e.preventDefault();
-        
-                var com_id = $(this).attr('id').split('-')[1];
-                var url = "/comment/" + com_id + "/delete";
-        
-                $.get($SCRIPT_ROOT + url, 
-                function(data, success) {
-                    // alert('deleted');
-                    $("#c" + com_id).remove();
-                });
-            });
-        });
-    });
+    $(".post-comment").click(comment_handler);
 
     // delete a comment 
-    $(".delete-comment").click(function(e) {
-        e.preventDefault();
-
-        var com_id = $(this).attr('id').split('-')[1];
-        var url = "/comment/" + com_id + "/delete";
-
-        $.get($SCRIPT_ROOT + url, 
-        function(data, success) {
-            // alert('deleted');
-            $("#c" + com_id).remove();
-        });
-    });
+    $(".delete-comment").click(delete_comment);
 });
