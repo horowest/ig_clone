55bca7865959f2c3dfeef6e2f75130cfb48bfce4
horowest <akashvevo5@gmail.com>
2020-06-13 20:23:15 +0530

    sqlite db

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -5,8 +5,10 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '2951b9d5bb58fe1fac16d872533168aa'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+if not os.path.exists('site.db'):
+    db.create_all()
 
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
